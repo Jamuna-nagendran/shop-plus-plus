7f13bf57031424a57144e8acb51ed1795cadd725
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2023-12-19 12:48:36 +0530

    seventh commit

--- app/src/pages/components/HomePageComponent.js
+++ app/src/pages/components/HomePageComponent.js
@@ -36,10 +36,15 @@ const HomePageComponent = ({ categories, getBestsellers }) => {
     <>
       <MetaComponent />
       {loading ? (
-        // Display loading spinner while data is being fetched
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
+        // Display centered loading spinner
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "80vh" }}
+        >
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Container>
       ) : (
         <>
           <ProductCarouselComponent bestSellers={bestSellers} />
