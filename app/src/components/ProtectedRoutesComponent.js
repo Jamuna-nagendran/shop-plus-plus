1f46f3a52259bed60badc6ba0642eabce2786556
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2023-12-19 12:32:38 +0530

    sixth commit

--- app/src/components/ProtectedRoutesComponent.js
+++ app/src/components/ProtectedRoutesComponent.js
@@ -9,20 +9,13 @@ const ProtectedRoutesComponent = ({ admin }) => {
   const [isAuth, setIsAuth] = useState();
 
   useEffect(() => {
-    axios
-      .get("/api/get-token")
-      .then(function (data) {
-        if (data.data.token) {
-          setIsAuth(data.data.token);
-        }
-      })
-      .catch(function (error) {
-        // Handle unauthorized error
-        if (error.response && error.response.status === 401) {
-          setIsAuth(undefined); // or set it to undefined or false based on your logic
-        }
-      });
-  }, []);
+    axios.get("/api/get-token").then(function (data) {
+      if (data.data.token) {
+        setIsAuth(data.data.token);
+      }
+      return isAuth;
+    });
+  }, [isAuth]);
 
   if (isAuth === undefined) return <LoginPage />;
 
--- app/src/pages/components/HomePageComponent.js
+++ app/src/pages/components/HomePageComponent.js
@@ -1,40 +1,62 @@
 import ProductCarouselComponent from "../../components/ProductCarouselComponent";
 import CategoryCardComponent from "../../components/CategoryCardComponent";
-import { Row, Container } from "react-bootstrap";
+import { Row, Container, Alert, Spinner } from "react-bootstrap";
 
 import { useEffect, useState } from "react";
 import MetaComponent from "../../components/MetaComponent";
 
 const HomePageComponent = ({ categories, getBestsellers }) => {
-  
-    const [mainCategories, setMainCategories] = useState([]);
-    const [bestSellers, setBestsellers] = useState([]);
-    const [error, setError] = useState('');
+  const [mainCategories, setMainCategories] = useState([]);
+  const [bestSellers, setBestsellers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-        getBestsellers()
-        .then((data) => {
-            setBestsellers(data);
-        })
-        .catch((er) => {
-            setError(er.response.data.message ? er.response.data.message : er.response.data)
-           console.log(er.response.data.message ? er.response.data.message : er.response.data) 
-        });
-        setMainCategories((cat) => categories.filter((item) => !item.name.includes("/")));
-    }, [categories])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const bestSellersData = await getBestsellers();
+        setBestsellers(bestSellersData);
+
+        const mainCategoriesData = categories.filter(
+          (item) => !item.name.includes("/")
+        );
+        setMainCategories(mainCategoriesData);
+        setLoading(false); // Set loading to false after data is fetched
+      } catch (err) {
+        setError(err.response?.data?.message || "An error occurred");
+        setLoading(false); // Set loading to false in case of an error
+        console.error(err.response?.data?.message || "An error occurred", err);
+      }
+    };
+
+    fetchData();
+  }, [categories, getBestsellers]);
 
   return (
     <>
-    <MetaComponent />
-      <ProductCarouselComponent bestSellers={bestSellers} />
-      <Container>
-        <Row xs={1} md={2} className="g-4 mt-5">
-          {mainCategories.map((category, idx) => (
-            <CategoryCardComponent key={idx} category={category} idx={idx} />
-          ))}
-        </Row>
-        {error}
-      </Container>
+      <MetaComponent />
+      {loading ? (
+        // Display loading spinner while data is being fetched
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      ) : (
+        <>
+          <ProductCarouselComponent bestSellers={bestSellers} />
+          <Container>
+            <Row xs={1} md={2} className="g-4 mt-5">
+              {mainCategories.map((category, idx) => (
+                <CategoryCardComponent
+                  key={idx}
+                  category={category}
+                  idx={idx}
+                />
+              ))}
+            </Row>
+            {error && <Alert variant="danger">{error}</Alert>}
+          </Container>
+        </>
+      )}
     </>
   );
 };
