20ba1c12cc172ba003d92f729b00ff03e8dfcb76
Jamuna-nagendran <jamuna.nagendran@gmail.com>
2023-12-19 19:57:46 +0530

    product carousel

--- app/src/components/ProductCarouselComponent.js
+++ app/src/components/ProductCarouselComponent.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import LazyLoad from "react-lazyload";
 
 const ProductCarouselComponent = ({ bestSellers }) => {
-  const [textColor, setTextColor] = useState("white");
-  const [backgroundColor, setBackgroundColor] = useState("rgba(0, 0, 0, 0.5)");
+  const [textColor, setTextColor] = useState("white"); // Default text color
+  const [backgroundColor, setBackgroundColor] = useState("rgba(0, 0, 0, 0.5)"); // Default background color
 
   useEffect(() => {
     const calculateContrast = (r, g, b) => {
@@ -23,7 +22,7 @@ const ProductCarouselComponent = ({ bestSellers }) => {
       img.onload = () => {
         const canvas = document.createElement("canvas");
         const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, 1, 1);
+        ctx.drawImage(img, 0, 0, 1, 1); // Draw a 1x1 pixel image
 
         const [r, g, b] = ctx.getImageData(0, 0, 1, 1).data;
         const contrastType = calculateContrast(r, g, b);
@@ -42,19 +41,13 @@ const ProductCarouselComponent = ({ bestSellers }) => {
     <Carousel style={{ height: "550px", overflow: "hidden" }}>
       {bestSellers.map((item, idx) => (
         <Carousel.Item key={idx}>
-          <LazyLoad height={500} offset={100}>
-            <img
-              crossOrigin="anonymous"
-              className="d-block w-100"
-              style={{
-                objectFit: "cover",
-                height: "500px",
-                maxHeight: "500px",
-              }}
-              src={item.images ? item.images[0].path : null}
-              alt={`Slide ${idx + 1}`}
-            />
-          </LazyLoad>
+          <img
+            crossOrigin="anonymous"
+            className="d-block w-100"
+            style={{ objectFit: "cover", height: "500px", maxHeight: "500px" }}
+            src={item.images ? item.images[0].path : null}
+            alt={`Slide ${idx + 1}`}
+          />
           <Carousel.Caption
             style={{ color: textColor, backgroundColor: backgroundColor }}
           >
